144b700ab2ca0c636969ad148335de750a0b5be6
ridelaos-master <ridelaosapp@gmail.com>
2026-02-26 06:53:34 +0700

    Add safe-area padding styles and integrate MainLayout in App component for improved routing structure

--- english-master/src/App.tsx
+++ english-master/src/App.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
+import MainLayout from './components/MainLayout';
 import RideHomePage from './pages/RideHomePage';
 import CoursesPage from './pages/CoursesPage';
 import CourseDetailPage from './pages/CourseDetailPage';
 import AuthCallbackPage from './pages/AuthCallbackPage';
+import BookingsPage from './pages/BookingsPage';
 import BookingPage from './pages/BookingPage';
 import PaymentPage from './pages/PaymentPage';
 import CompletePage from './pages/CompletePage';
@@ -12,55 +14,65 @@ import SafetyPage from './pages/SafetyPage';
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#F5F5F5]">
+    <div className="min-h-screen bg-rl-bg">
       <Routes>
-        {/* 공개 */}
-        <Route path="/" element={<RideHomePage />} />
-        <Route path="/courses" element={<CoursesPage />} />
-        <Route path="/courses/:id" element={<CourseDetailPage />} />
-        <Route path="/auth/callback" element={<AuthCallbackPage />} />
+        <Route element={<MainLayout />}>
+          {/* 공개 */}
+          <Route path="/" element={<RideHomePage />} />
+          <Route path="/courses" element={<CoursesPage />} />
+          <Route path="/courses/:id" element={<CourseDetailPage />} />
+          <Route path="/auth/callback" element={<AuthCallbackPage />} />
 
-        {/* 보호: 로그인 필요 */}
-        <Route
-          path="/booking/:id"
-          element={
-            <ProtectedRoute>
-              <BookingPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/payment"
-          element={
-            <ProtectedRoute>
-              <PaymentPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/complete"
-          element={
-            <ProtectedRoute>
-              <CompletePage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/mypage"
-          element={
-            <ProtectedRoute>
-              <MypagePage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/safety"
-          element={
-            <ProtectedRoute>
-              <SafetyPage />
-            </ProtectedRoute>
-          }
-        />
+          {/* 보호: 로그인 필요 */}
+          <Route
+            path="/bookings"
+            element={
+              <ProtectedRoute>
+                <BookingsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/booking/:id"
+            element={
+              <ProtectedRoute>
+                <BookingPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/payment"
+            element={
+              <ProtectedRoute>
+                <PaymentPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/complete"
+            element={
+              <ProtectedRoute>
+                <CompletePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/mypage"
+            element={
+              <ProtectedRoute>
+                <MypagePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/safety"
+            element={
+              <ProtectedRoute>
+                <SafetyPage />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
       </Routes>
     </div>
   );
